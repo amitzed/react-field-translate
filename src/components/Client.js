8c9b1a783971b00a109a58cca8422a52d48ce136
amitzed <amitzmn@gmail.com>
2020-06-15 15:58:02 -0400

    Created functional component in Client file that will be parent to Name.js and SubmitBtn.js

---
+++ src/components/Client.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Name from './Name';
+import SubmitBtn from './SubmitBtn';
+
+const Client = () => {
+  return (
+    <div className="ui form">
+      <Name />
+      <SubmitBtn />
+    </div>
+  )
+}
+
+export default Client;
