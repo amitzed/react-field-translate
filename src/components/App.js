1943bf2769049c0d99fb5fed97835cec9efb3f68
amitzed <amitzmn@gmail.com>
2020-06-15 15:05:59 -0400

    Initialized state with default english language and added flag icons of several different countries

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,19 @@
 import React from 'react';
 
 class App extends React.Component {
+  state = { language: 'english'};
+
   render() {
     return (
-      <div>App</div>
+      <div className="ui container">
+        <div>
+          <i className="flag us" />
+          <i className="flag bd" />
+          <i className="flag bw" />
+          <i className="flag de" />
+          <i className="flag hk" />
+        </div>
+      </div>
     )
   }
 }
