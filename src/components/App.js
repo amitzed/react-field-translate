cb3e4ce6196ca749fd24c50b09fe98e662909c3f
amitzed <amitzmn@gmail.com>
2020-06-15 15:29:41 -0400

    Added click event handler to diff languages

--- src/components/App.js
+++ src/components/App.js
@@ -4,15 +4,19 @@ import React from 'react';
 class App extends React.Component {
   state = { language: 'english'};
 
+  onLanguageChange = (language) => {
+    this.setState({ language })
+  };
+
   render() {
     return (
       <div className="ui container">
         <div className="wrapper">
-          <i className="flag us" />
-          <i className="flag bd" />
-          <i className="flag bw" />
-          <i className="flag de" />
-          <i className="flag hk" />
+          <i className="flag us" onClick={()=> this.onLanguageChange('english')} />
+          <i className="flag bd" onClick={()=> this.onLanguageChange('bengali')}  />
+          <i className="flag bw" onClick={()=> this.onLanguageChange('botswana')}  />
+          <i className="flag de" onClick={()=> this.onLanguageChange('german')}  />
+          <i className="flag hk" onClick={()=> this.onLanguageChange('hongkong')}  />
         </div>
       </div>
     )
